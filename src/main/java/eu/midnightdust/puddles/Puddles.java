e71c9035c6deebd031ebbcd719d40557bd0f9415
Redlemon <redlemonmailbox@gmail.com>
2024-01-26 12:27:52 +0100

    updated

--- src/main/java/eu/midnightdust/puddles/Puddles.java
+++ src/main/java/eu/midnightdust/puddles/Puddles.java
@@ -3,17 +3,15 @@ package eu.midnightdust.puddles;
 import eu.midnightdust.puddles.block.PuddleBlock;
 import eu.midnightdust.puddles.config.PuddlesConfig;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.gamerule.v1.GameRuleFactory;
-import net.fabricmc.fabric.api.gamerule.v1.GameRuleRegistry;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.Material;
 import net.minecraft.fluid.Fluids;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
-import net.minecraft.world.GameRules;
+import net.minecraft.registry.Registry;
 
 public class Puddles implements ModInitializer {
     public static final String MOD_ID = "puddles";
@@ -21,7 +19,7 @@ public class Puddles implements ModInitializer {
 
     public void onInitialize() {
         PuddlesConfig.init(MOD_ID, PuddlesConfig.class);
-        Registry.register(Registry.BLOCK, new Identifier(MOD_ID,"puddle"), Puddle);
-        Registry.register(Registry.ITEM, new Identifier(MOD_ID,"puddle"), new BlockItem(Puddle, new Item.Settings()));
+        Registry.register(Registries.BLOCK, new Identifier(MOD_ID,"puddle"), Puddle);
+        Registry.register(Registries.ITEM, new Identifier(MOD_ID,"puddle"), new BlockItem(Puddle, new Item.Settings()));
     }
 }
